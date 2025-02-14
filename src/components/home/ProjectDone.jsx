86a463f5cfce7eb4ddb31f25c717c20670b529e6
Ashutosh Kumar <86124272+Ashutosh-kumar-code@users.noreply.github.com>
2025-02-14 11:55:56 +0530

    Update ProjectDone.jsx

--- src/components/home/ProjectDone.jsx
+++ src/components/home/ProjectDone.jsx
@@ -19,47 +19,47 @@ const ProjectDone = ({ refs }) => {
   const navigate = useNavigate()
 
   const data = [
+    // {
+    //   skill: "Web Developer",
+    //   web_image: supply_web1,
+    //   topic_name: "SupplyArabia",
+    //   description: "In SupplyArabia, its leading the charge in revolutionizing the online marketplace exclusively dedicated to the supply of raw materials to the Middle East and Arabic-speaking countries."
+    //   , live_link: "https://supplyarabia.infosparkles.com/"
+    // },
+    // {
+    //   skill: "Web Developer",
+    //   web_image: online_shop,
+    //   topic_name: "Online Grocery Shop",
+    //   description: "This is an online grocery store designed to provide customers with a convenient and seamless shopping experience. It offers a wide range of fresh produce, dairy products, and household essentials delivered directly to your doorstep."
+    //   , live_link: "https://grocery-onlineshop.netlify.app/"
+    // },
+    // {
+    //   skill: "",
+    //   web_image: lms_web1,
+    //   topic_name: "Video LMS",
+    //   description: "Video LMS is an educational platform designed to deliver video-based learning content.Video LMS aims to enhance digital learning experiences for both instructors and students."
+    //   , live_link: "https://lms.infosparkles.com/"
+    // },
+    // {
+    //   skill: "",
+    //   web_image: deal_web1,
+    //   topic_name: "DeaLin",
+    //   description: "DealIn is a platform designed for users to buy and sell properties, vehicles, and a variety of products. It provides a marketplace where individuals and businesses can list their items for sale, and buyers can browse and filter through listings to find what they need."
+    //   , live_link: "http://dealin.infosparkles.com/"
+    // }, {
+    //   skill: "",
+    //   web_image: desert_web1,
+    //   topic_name: "DesertGate",
+    //   description: "This projects is to provide peoples the facilities to book their destination tours. Also it includes hotel booking and city tours."
+    //   , live_link: "https://desert2.infosparkles.com/"
+    // },
     {
-      skill: "Web Developer",
-      web_image: supply_web1,
-      topic_name: "SupplyArabia",
-      description: "In SupplyArabia, its leading the charge in revolutionizing the online marketplace exclusively dedicated to the supply of raw materials to the Middle East and Arabic-speaking countries."
-      , live_link: "https://supplyarabia.infosparkles.com/"
-    },
-    {
-      skill: "Web Developer",
-      web_image: online_shop,
-      topic_name: "Online Grocery Shop",
-      description: "This is an online grocery store designed to provide customers with a convenient and seamless shopping experience. It offers a wide range of fresh produce, dairy products, and household essentials delivered directly to your doorstep."
-      , live_link: "https://grocery-onlineshop.netlify.app/"
-    },
-    {
-      skill: "",
-      web_image: lms_web1,
-      topic_name: "Video LMS",
-      description: "Video LMS is an educational platform designed to deliver video-based learning content.Video LMS aims to enhance digital learning experiences for both instructors and students."
-      , live_link: "https://lms.infosparkles.com/"
-    },
-    {
-      skill: "",
-      web_image: deal_web1,
-      topic_name: "DeaLin",
-      description: "DealIn is a platform designed for users to buy and sell properties, vehicles, and a variety of products. It provides a marketplace where individuals and businesses can list their items for sale, and buyers can browse and filter through listings to find what they need."
-      , live_link: "http://dealin.infosparkles.com/"
-    }, {
-      skill: "",
-      web_image: desert_web1,
-      topic_name: "DesertGate",
-      description: "This projects is to provide peoples the facilities to book their destination tours. Also it includes hotel booking and city tours."
-      , live_link: "https://desert2.infosparkles.com/"
-    },
-    {
-      skill: "",
-      web_image: budget_book,
-      topic_name: "Budget Booking",
-      description: "BudgetBooking is an innovative platform designed to help travelers find and book hotels within their budget. It features exclusive discounts and deals, ensuring users get the best value for their money while enjoying comfortable accommodations"
-      , live_link: "https://budgetbooking.infosparkles.com/"
-    },
+    //   skill: "",
+    //   web_image: budget_book,
+    //   topic_name: "Budget Booking",
+    //   description: "BudgetBooking is an innovative platform designed to help travelers find and book hotels within their budget. It features exclusive discounts and deals, ensuring users get the best value for their money while enjoying comfortable accommodations"
+    //   , live_link: "https://budgetbooking.infosparkles.com/"
+    // },
     //    {
     // skill: "",
     // web_image: homeizon_web1,
