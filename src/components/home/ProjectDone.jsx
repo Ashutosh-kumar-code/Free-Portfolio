6dd151ef7ceaf81566672e5ef8dd209d1231e9fb
Ashutosh-kumar-code <ashu9908kumar@gmail.com>
2025-09-17 00:31:50 +0530

    remove proectj

--- src/components/home/ProjectDone.jsx
+++ src/components/home/ProjectDone.jsx
@@ -20,13 +20,13 @@ const ProjectDone = ({ refs }) => {
   // const navigate = useNavigate()
 
   const data = [
-    {
-      skill: "Web Developer",
-      web_image: supply_web1,
-      topic_name: "SupplyArabia",
-      description: "In SupplyArabia, its leading the charge in revolutionizing the online marketplace exclusively dedicated to the supply of raw materials to the Middle East and Arabic-speaking countries."
-      , live_link: "https://supplyarabia.infosparkles.com/"
-    },
+    // {
+    //   skill: "Web Developer",
+    //   web_image: supply_web1,
+    //   topic_name: "SupplyArabia",
+    //   description: "In SupplyArabia, its leading the charge in revolutionizing the online marketplace exclusively dedicated to the supply of raw materials to the Middle East and Arabic-speaking countries."
+    //   , live_link: "https://supplyarabia.infosparkles.com/"
+    // },
     {
       skill: "Web Developer",
       web_image: online_shop,
@@ -41,13 +41,14 @@ const ProjectDone = ({ refs }) => {
       description: "Video LMS is an educational platform designed to deliver video-based learning content.Video LMS aims to enhance digital learning experiences for both instructors and students."
       , live_link: "https://lms.infosparkles.com/"
     },
-    {
-      skill: "",
-      web_image: deal_web1,
-      topic_name: "DeaLin",
-      description: "DealIn is a platform designed for users to buy and sell properties, vehicles, and a variety of products. It provides a marketplace where individuals and businesses can list their items for sale, and buyers can browse and filter through listings to find what they need."
-      , live_link: "http://dealin.infosparkles.com/"
-    }, {
+    // {
+    //   skill: "",
+    //   web_image: deal_web1,
+    //   topic_name: "DeaLin",
+    //   description: "DealIn is a platform designed for users to buy and sell properties, vehicles, and a variety of products. It provides a marketplace where individuals and businesses can list their items for sale, and buyers can browse and filter through listings to find what they need."
+    //   , live_link: "http://dealin.infosparkles.com/"
+    // },
+     {
       skill: "",
       web_image: desert_web1,
       topic_name: "DesertGate",
